08ba3459185edea4fbbfac738ff741f534a6e54a
Ayelet <ayeletcb@gmail.com>
2019-09-26 19:21:56 -0400

    moved playIntro to new class

--- src/piano/Main.java
+++ src/piano/Main.java
@@ -9,33 +9,8 @@ public class Main {
             MidiChannel midiChannel = new Channel().getChannel();
             PianoGUI frame = new PianoGUI(midiChannel);
             frame.setVisible(true);
-            playIntro(midiChannel);
+            new Intro().playIntro(midiChannel);
         } catch (MidiUnavailableException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void playIntro(MidiChannel channel)
-    {
-        try
-        {
-            int[] notes = { Notes.C, Notes.D, Notes.E, Notes.F, Notes.G, Notes.A, Notes.B };
-            for (int i = 0; i < notes.length; ++i)
-            {
-                channel.noteOn(notes[i], SoundSettings.VOLUME);
-                Thread.sleep(100);
-                channel.noteOff(notes[i]);
-            }
-            // Play a C major chord.
-            channel.noteOn(Notes.C, SoundSettings.VOLUME);
-            channel.noteOn(Notes.E, SoundSettings.VOLUME);
-            channel.noteOn(Notes.G, SoundSettings.VOLUME);
-            Thread.sleep(3000);
-            channel.allNotesOff();
-            Thread.sleep(500);
-        }
-        catch (InterruptedException e)
-        {
             e.printStackTrace();
         }
     }
